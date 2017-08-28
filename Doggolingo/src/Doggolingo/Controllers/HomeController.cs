ab67f6010aee1d800922d0b9396c76eb7884ea81
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-28 16:44:37 -0700

    initial project set up and layout, style first image and title

---
+++ Doggolingo/src/Doggolingo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Doggolingo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
