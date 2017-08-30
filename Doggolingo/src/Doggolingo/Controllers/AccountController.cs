167c4c3ea5fba6632cf1f267b283fec7a224511c
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-30 08:20:56 -0700

    add register/login/logoff functionality to account controller; add link in _Layout to those functions

---
+++ Doggolingo/src/Doggolingo/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Doggolingo.Models;
+using Microsoft.AspNetCore.Identity;
+using Doggolingo.ViewModels;
+
+namespace Doggolingo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private DoggolingoDbContext db = new DoggolingoDbContext();
+
+        private readonly DoggolingoDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, DoggolingoDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        { //when user profiles are created, this will return a view with that user's profile passed in as the model
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            ApplicationUser user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return await LoginNewUser(user, model.Password);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> LoginNewUser(ApplicationUser user, string password)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(user.UserName, password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return RedirectToAction("Register");
+            }
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
