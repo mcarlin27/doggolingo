1ba37f357cbb8dd8b2c5e494e3e2c1308d2287d0
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-31 11:23:33 -0700

    add Module and Lesson classes

---
+++ Doggolingo/src/Doggolingo/Models/Lesson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggolingo.Models
+{
+    public class Lesson
+    {
+        [Key]
+        public int LessonId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public virtual Module Module { get; set; }
+        public ICollection <Step> Steps { get; set; }
+
+        public Lesson()
+        {
+        }
+    }
+}
---
+++ Doggolingo/src/Doggolingo/Models/Module.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggolingo.Models
+{
+    public class Module
+    {
+        [Key]
+        public int ModuleId { get; set; }
+        public string Title { get; set; }
+        public int Treats { get; set; }
+        public virtual ICollection<Lesson> Lessons { get; set; }
+        public virtual ICollection<DogParent> DogParents { get; set; }
+
+        public Module()
+        {
+            Treats = 10;
+        }
+    }
+}
