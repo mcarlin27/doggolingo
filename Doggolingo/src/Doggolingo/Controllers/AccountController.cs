dad2ca9033858c93f8ccfd5ef660e5c7c5be2c17
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-31 13:20:00 -0700

    adjust Lessons Details, Modules Index, and Shared _Layout views

--- Doggolingo/src/Doggolingo/Controllers/AccountController.cs
+++ Doggolingo/src/Doggolingo/Controllers/AccountController.cs
@@ -74,7 +74,7 @@ namespace Doggolingo.Controllers
             Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
             if (result.Succeeded)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
             }
             else
             {
---
+++ Doggolingo/src/Doggolingo/Controllers/LessonsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Doggolingo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Doggolingo.Controllers
+{
+    public class LessonsController : Controller
+    {
+        private DoggolingoDbContext db = new DoggolingoDbContext();
+
+        public IActionResult Details(int id)
+        {
+            Lesson thisLesson = db.Lessons.Include(l => l.Module)
+                                          .Include(l => l.Steps)
+                                          .FirstOrDefault(l => l.LessonId == id);
+            return View(thisLesson);
+        }
+    }
+}
