cebc2ef677727bfbf059f8d0a68f18eee7de56c4
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-08-31 14:21:52 -0700

    add MarkComplete method to ModulesController to mark modules as complete for individual dogparents

--- Doggolingo/src/Doggolingo/Controllers/ModulesController.cs
+++ Doggolingo/src/Doggolingo/Controllers/ModulesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Doggolingo.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Doggolingo.Controllers
 {
@@ -22,6 +23,18 @@ namespace Doggolingo.Controllers
             Module thisModule = db.Modules.Include(m => m.Lessons)
                                           .FirstOrDefault(m => m.ModuleId == id);
             return View(thisModule);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult MarkComplete(int moduleId)
+        {
+            Module thisModule = db.Modules.FirstOrDefault(m => m.ModuleId == moduleId);
+            DogParent thisDogParent = db.DogParents.FirstOrDefault(d => d.UserName == User.Identity.Name);
+            thisDogParent.CompletedModules.Add(thisModule);
+            db.Entry(thisDogParent).State = EntityState.Modified;
+            db.SaveChanges();
+            return View("Index", "Modules");
         }
     }
 }
